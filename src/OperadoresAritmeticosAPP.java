15afd6f07809edd6658510da07fd7c9823c29060
Abel-Git2103 <abel_jornet@hotmail.com>
2022-01-11 23:54:24 +0100

    PRIMER COMMIT

---
+++ src/OperadoresAritmeticosAPP.java
@@ -0,0 +1,13 @@
+
+public class OperadoresAritmeticosAPP {
+
+	public static void main(String[] args) {
+		
+		int variableA = 20, variableB = 20;
+		System.out.println("La suma es igual a " + (variableA + variableB));
+		System.out.println("La resta es igual a " + (variableA - variableB));
+		System.out.println("La multiplicación es igual a " + (variableA * variableB));
+		System.out.println("La division es igual a " + (variableA / variableB));
+		System.out.println("El modulo es igual a " + (variableA % variableB));
+	}
+}
